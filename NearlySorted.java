0905e3d99252325b4b266629c6516fd4b36a472a
Swarnadeep Sahani <swarnadeepsahani277@gmail.com>
2023-11-17 13:13:41 +0530

    Add files via upload

---
+++ NearlySorted.java
@@ -0,0 +1,39 @@
+/*
+ Given a sorted array arr[] of size N, some elements of array are moved to either of the adjacent positions, i.e., arr[i] may be present at arr[i+1] or arr[i-1] i.e. arr[i] can only be swapped with either arr[i+1] or arr[i-1]. The task is to search for an element in this array.
+
+Examples : 
+
+Input: arr[] =  {10, 3, 40, 20, 50, 80, 70}, key = 40
+Output: 2 
+Explanation: Output is index of 40 in given array i.e. 2
+
+Input: arr[] =  {10, 3, 40, 20, 50, 80, 70}, key = 90
+Output: -1
+Explanation: -1 is returned to indicate the element is not present
+ */
+class NearlySorted {
+        static int search(int arr[],int target)
+        {
+            int start=0;
+            int end=arr.length-1;
+            while (start<=end)
+            {
+                int mid=start+(end-start)/2;
+                if(arr[mid]==target)
+                   return mid;
+                   if ((mid-1>=start) && arr[mid-1]==target)
+                        return mid-1;
+                    if ((mid+1<=end) && arr[mid+1]==target)
+                        return mid+1;
+                        if(arr[mid]>target)
+                              end=mid-2;
+                              else if(arr[mid]<target)
+                                start=mid+2;
+            }
+            return -1;
+        }
+        public static void main(String[] args) {
+            int arr[] =  {10, 3, 40, 20, 50, 80, 70};
+            System.out.println(search(arr, 90));
+        }
+}
